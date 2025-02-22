7546e0fff7bf52642986d0a03c953b2d954de224
Max Maier <maxmaierxci@icloud.com>
2025-02-22 16:45:41 +0500

    Add files via upload

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,60 @@
+describe('Поверка авторизации', function () {
+
+    it('Верный логин и верный пароль', function () {
+         cy.visit('https://login.qa.studio/');
+         cy.get('#mail').type('german@dolnikov.ru');
+         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#loginButton').click();
+         cy.get('#messageHeader').should('be.visible');
+         cy.get('#messageHeader').contains('Авторизация прошла успешно');
+         cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+     })
+
+     it('Восстановление пароля', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Верный логин и неверный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio0');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Неверный логин и верный пароль', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#mail').type('german@dolnikov0.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+
+    it('Валидация на наличие @', function () {
+        cy.visit('https://login.qa.studio');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+    })
+
+    it('Проверка на приведение к строчным буквам в логине', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    })
+ }) 
---
+++ cypress/e2e/pokemonbattle.cy.js
@@ -0,0 +1,22 @@
+describe('Покупка аватара в Битве Покемонов', function () {
+
+    it('Тест на покупку аватара', function () {
+         cy.visit('https://pokemonbattle.ru/login');
+         cy.get(':nth-child(1) > .auth__input').type('USER_LOGIN');
+         cy.get('#password').type('USER_PASSWORD');
+         cy.get('.auth__button').click();
+         cy.get('.header__container > .header__id').click();
+         cy.get('[href="/shop"]').click();
+         cy.get('.available > button').first().click();
+         cy.get('.pay__payform-v2 > :nth-child(2) > .pay_base-input-v2').type('4620869113632996');
+         cy.get(':nth-child(1) > .pay_base-input-v2').type('1225');
+         cy.get('.pay-inputs-box > :nth-child(2) > .pay_base-input-v2').type('125');
+         cy.get('.pay__input-box-last-of > .pay_base-input-v2').type('NAME');
+         cy.get('.pay-btn').click();
+         cy.get('#cardnumber').type('56456');
+         cy.get('.payment__submit-button').click();
+         cy.contains('Покупка прошла успешно').should('be.visible');
+         cy.get('.payment__adv').click();
+         cy.get('.header__container > .header__id').click();
+     })
+ }) 
